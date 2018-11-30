af22fb00ef59ef827e7df5685e34ab9ea1e26d74
“y“yyyw” <243574589@qq.com>
2018-11-30 16:32:57 +0800

    结束第五个视频的学习 文章列表

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Article
 
 # Create your views here.
+def article_detail(request,article_id):
+    article =get_object_or_404(Article,pk=article_id)
+    context = {}
+    context['article_obj']=article
+    return render_to_response("article_detail.html",context)
+
+def article_list(request):
+    articles = Article.objects.all()
+    context = {}
+    context['articles']=articles
+    return render_to_response("article_list.html",context)
